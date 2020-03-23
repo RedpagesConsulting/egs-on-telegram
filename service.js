8c22c7208507ef18a6a900f7ec8cf02a716f00ab
Edidiong <edidiong.aligbe@gmail.com>
2020-03-23 15:18:37 +0100

    Update service.js

--- service.js
+++ service.js
@@ -46,7 +46,7 @@ module.exports = app => {
         
         idx++;
        
-        if (idx == 3) {
+        if (idx == 2) {
           idx = 0;
           keyboard.push(keyboard3);
           keyboard3 = [];
